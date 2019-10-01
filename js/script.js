c16f10d9ade9c076ec18db173099ba5092ff00d9
Bethany <blmeier@gmail.com>
2019-10-01 16:52:04 -0400

    work on showing tabs on click

--- js/script.js
+++ js/script.js
@@ -7,4 +7,22 @@ $(document).ready(function() {
             $('.nav').removeClass('solid');
         }
     });
+
+    // tabbed content section
+    let tabs = document.querySelectorAll('.title');
+    let panels = document.getElementsByClassName('panel');
+
+    // Add or remove active class on click (need to remove class when another tab is clicked)
+    tabs[0].addEventListener('click', function() {
+        console.log("You clicked the first tab");
+        $(this).toggleClass('active');
+    });
+    tabs[1].addEventListener('click', function() {
+        console.log("You clicked the second tab");
+        $(this).toggleClass('active');
+    });
+    tabs[2].addEventListener('click', function() {
+        console.log("You clicked the third tab");
+        $(this).toggleClass('active');
+    });
 })

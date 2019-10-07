8e5ccae4da890c8227551a5bfd4bc29b5e25fe19
Bethany <blmeier@gmail.com>
2019-10-07 16:03:57 -0400

    tabbed content JS

--- js/script.js
+++ js/script.js
@@ -9,20 +9,40 @@ $(document).ready(function() {
     });
 
     // tabbed content section
-    let tabs = document.querySelectorAll('.title');
-    let panels = document.getElementsByClassName('panel');
+    var tab1 = document.getElementById('tab1');
+    var tab2 = document.getElementById('tab2');
+    var tab3 = document.getElementById('tab3');
+    var tab1Content = document.getElementById('tab1-content');
+    var tab2Content = document.getElementById('tab2-content');
+    var tab3Content = document.getElementById('tab3-content');
 
-    // Add or remove active class on click (need to remove class when another tab is clicked)
-    tabs[0].addEventListener('click', function() {
-        console.log("You clicked the first tab");
-        $(this).toggleClass('active');
+    tab1.addEventListener('click', function() {
+        console.log('You clicked tab 1');
+        tab1.classList.add('active');
+        tab2.classList.remove('active');
+        tab3.classList.remove('active');
+        tab1Content.style.display = 'block';
+        tab2Content.style.display = 'none';
+        tab3Content.style.display = 'none';
     });
-    tabs[1].addEventListener('click', function() {
-        console.log("You clicked the second tab");
-        $(this).toggleClass('active');
+
+    tab2.addEventListener('click', function() {
+        console.log('You clicked tab 2');
+        tab1.classList.remove('active');
+        tab2.classList.add('active');
+        tab3.classList.remove('active');
+        tab1Content.style.display = 'none';
+        tab2Content.style.display = 'block';
+        tab3Content.style.display = 'none';
     });
-    tabs[2].addEventListener('click', function() {
-        console.log("You clicked the third tab");
-        $(this).toggleClass('active');
+
+    tab3.addEventListener('click', function() {
+        console.log('You clicked tab 3');
+        tab1.classList.remove('active');
+        tab2.classList.remove('active');
+        tab3.classList.add('active');
+        tab1Content.style.display = 'none';
+        tab2Content.style.display = 'none';
+        tab3Content.style.display = 'block';
     });
 })
